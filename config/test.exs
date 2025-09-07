40ba5813182ccacdbc3e8c03950da46e7710c0fc
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2025-09-08 06:44:09 +1000

    test: fix testdb configs

--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :linovc_core, LinovcCore.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "root",
+  password: "toor",
   hostname: "localhost",
   database: "linovc_core_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
