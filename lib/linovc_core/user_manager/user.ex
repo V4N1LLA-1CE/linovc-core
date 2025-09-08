2b605635f989b01dd28b962e0e55e2318af33282
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2025-09-08 12:47:31 +1000

    chore(db): schema and migration

---
+++ lib/linovc_core/user_manager/user.ex
@@ -0,0 +1,28 @@
+defmodule LinovcCore.UserManager.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :email, :string
+    field :password, :string
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:email, :password])
+    |> validate_required([:email, :password])
+    |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/, message: "must be a valid email")
+    |> validate_length(:password, min: 8, message: "must be at least 8 characters")
+    |> unique_constraint(:email)
+    |> hash_password()
+  end
+
+  defp hash_password(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, password: Argon2.hash_pwd_salt(password))
+  end
+
+  defp hash_password(changeset), do: changeset
+end
